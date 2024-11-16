5c279e4d1e8d5d4b45cb34dc0e35917d574339b4
Itamar-Manor <1manoritamar@gmail.com>
2024-11-16 21:45:45 +0200

    Simplified code

--- Cheers.java
+++ Cheers.java
@@ -1,22 +1,16 @@
 // Prints a crowd cheering output.
 public class Cheers {
-        public static String getPrefix(char c) {
-                char[] anLetters = { 'A', 'E', 'F', 'H', 'I', 'L', 'M', 'N', 'O', 'R', 'S', 'X' };
-                for (int i = 0; i < anLetters.length; i++) {
-                        if (c == anLetters[i]) {
-                                return "an";
-                        }
-                }
-                return "a ";
-        };
-
         public static void main(String[] args) {
                 String input = args[0].toUpperCase();
                 int times = Integer.parseInt(args[1]);
-
+                String anLetters = "AEFHILMNORSX";
                 for (int i = 0; i < input.length(); i++) {
                         char c = input.charAt(i);
-                        System.out.printf("Give me %s %c: %c!\n", getPrefix(c), c, c);
+                        if (anLetters.indexOf(c) == -1) {
+                                System.out.println("Give me a  " + c + ": " + c + "!");
+                        } else {
+                                System.out.println("Give me an " + c + ": " + c + "!");
+                        }
                 }
 
                 System.out.println("What does that spell?");
